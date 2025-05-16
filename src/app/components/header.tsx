92be0345c67acfd450adcc44b91092c4a50437ba
pedroeroel <pedroedroel@gmail.com>
2025-05-16 14:03:33 -0300

    1st

---
+++ src/app/components/header.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Header() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://unpkg.com/typewriter-effect@latest/dist/core.js";
+    script.async = true;
+    script.onload = () => {
+      // @ts-ignore
+      const Typewriter = window.Typewriter; 
+      if (Typewriter) {
+        const typewriterHelloElem = document.querySelector("#hello-typewriter");
+        // @ts-ignore
+        const typewriterHello = new Typewriter(typewriterHelloElem, {
+          loop: true,
+          delay: 75,
+        });
+        typewriterHello
+        .typeString("Hello, I'm Roel!")
+        .pauseFor(1500)
+        .deleteAll()
+        .typeString("Welcome to my portfolio!")
+        .pauseFor(1000)
+        .start();
+      }
+      const typewriterHelloWorldElem = document.querySelector("#hw-typewriter");
+      // @ts-ignore
+      
+      const typewriterHelloWorld = new Typewriter(typewriterHelloWorldElem, {
+        loop: true,
+        delay: 75,
+      });
+      typewriterHelloWorld
+      .typeString("console.log('Hello, World!')")
+      .pauseFor(1500)
+      .deleteAll()
+      .typeString("print('Hello, World!')")
+      .pauseFor(1000)
+      .start();
+      
+    };
+      document.body.appendChild(script);
+  return () => {
+    document.body.removeChild(script);
+  };
+}, []);
+
+return (
+  <header className="flex items-center justify-between p-4 bg-gray-100 text-gray-900">
+    <h1 className="text-xl font-bold w-1/3">&lt;<span className="text-blue-500">roel</span>.code /&gt;</h1>
+    <nav className="w-1/3">
+      <ul className="flex space-x-10 justify-center">
+        <li>
+          <a href="/" className="border-b-2 not-hover:border-transparent  transition duration-200 ease-in-out">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="border-b-2 not-hover:border-transparent  transition duration-200 ease-in-out">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="/projects" className="border-b-2 not-hover:border-transparent  transition duration-200 ease-in-out">
+           projects
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div
+      className="text-lg font-semibold text-gray-700 w-1/3"
+    >
+      <p id="hw-typewriter" className="text-right text-gray-400 m-0" style={{ minHeight: "1.5em" }}>
+        console.log("Hello, World!")
+      </p>
+    </div>
+  </header>
+);
